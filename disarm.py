37a80463eff0e742729b61693f1cf4b34b791e76
FrancoRosa <km115.franco@gmail.com>
2021-02-08 20:50:15 -0500

    Add control scripts

--- disarm.py
+++ disarm.py
@@ -1,5 +1,5 @@
 import serial
 port = 'COM10'
 ser = serial.Serial(port = port, rtscts = True, dsrdtr=True)  # open serial port
-ser.write(b'arm\n')     # write a string
+ser.write(b'disarm\n')     # write a string
 ser.close()             # close port
